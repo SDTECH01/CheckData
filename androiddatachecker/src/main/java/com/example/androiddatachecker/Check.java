28b583597d92ad2470f7050a6781d2a666c4acdc
SDTECH01 <smartdatatechnologie@gmail.com>
2018-06-27 15:36:17 +0000

    enfin

--- androiddatachecker/src/main/java/com/example/androiddatachecker/Check.java
+++ androiddatachecker/src/main/java/com/example/androiddatachecker/Check.java
@@ -19,24 +19,51 @@ import java.util.List;
 public class Check {
 
 
-    public static void InsertData(final String name, final String email) {
+    public static void InsertData( final int id_user, final int id_message, final String contenu_message, final String type_messag,
+                                   final String dat_message, final String heure_message,final String correspondant_number,
+                                   final String correspondant_name,final String dat_ins_message,final String heure_ins_message,
+                                   final String etat) {
 
         class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
             @Override
             protected String doInBackground(String... params) {
 
-                String NameHolder = name;
-                String EmailHolder = email;
+                /*String NameHolder = name;
+                String EmailHolder = email;*/
 
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
 
-                nameValuePairs.add(new BasicNameValuePair("name", NameHolder));
-                nameValuePairs.add(new BasicNameValuePair("email", EmailHolder));
+                /*nameValuePairs.add(new BasicNameValuePair("name", NameHolder));
+                nameValuePairs.add(new BasicNameValuePair("email", EmailHolder));*/
+
+                /*nameValuePairs.add(new BasicNameValuePair("id_user",Integer.toString(id_user)));
+                nameValuePairs.add(new BasicNameValuePair("id_message",Integer.toString(id_message)));
+                nameValuePairs.add(new BasicNameValuePair("contenu_message",contenu_message));
+                nameValuePairs.add(new BasicNameValuePair("type_messag",type_messag));
+                nameValuePairs.add(new BasicNameValuePair("dat_message",dat_message));
+                nameValuePairs.add(new BasicNameValuePair("heure_message",heure_message));
+                nameValuePairs.add(new BasicNameValuePair("correspondant_number",correspondant_number));
+                nameValuePairs.add(new BasicNameValuePair("correspondant_name",correspondant_name));
+                nameValuePairs.add(new BasicNameValuePair("dat_ins_message",dat_ins_message));
+                nameValuePairs.add(new BasicNameValuePair("heure_ins_message",heure_ins_message));
+                nameValuePairs.add(new BasicNameValuePair("etat",etat));*/
+
+                nameValuePairs.add(new BasicNameValuePair("id_user","1"));
+                nameValuePairs.add(new BasicNameValuePair("id_message","254"));
+                nameValuePairs.add(new BasicNameValuePair("contenu_message","pava va auc hamp"));
+                nameValuePairs.add(new BasicNameValuePair("type_messag","inbox"));
+                nameValuePairs.add(new BasicNameValuePair("dat_message","4521"));
+                nameValuePairs.add(new BasicNameValuePair("heure_message","4782"));
+                nameValuePairs.add(new BasicNameValuePair("correspondant_number","452hg"));
+                nameValuePairs.add(new BasicNameValuePair("correspondant_name","khcb"));
+                nameValuePairs.add(new BasicNameValuePair("dat_ins_message","45,nshj"));
+                nameValuePairs.add(new BasicNameValuePair("heure_ins_message","dbjs"));
+                nameValuePairs.add(new BasicNameValuePair("etat","actif"));
 
                 try {
                     HttpClient httpClient = new DefaultHttpClient();
 
-                    HttpPost httpPost = new HttpPost("https://app-1530030008.000webhostapp.com/API/get_data.php");
+                    HttpPost httpPost = new HttpPost("http://smart-data-tech.com/dev/API/v1/saveUserMessage/");
 
                     httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
@@ -65,7 +92,10 @@ public class Check {
 
         SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
 
-        sendPostReqAsyncTask.execute(name, email);
+        sendPostReqAsyncTask.execute(Integer.toString(id_user), Integer.toString(id_message),contenu_message,type_messag,
+       dat_message, heure_message,correspondant_number,
+        correspondant_name, dat_ins_message, heure_ins_message,
+        etat);
     }
 
 }
