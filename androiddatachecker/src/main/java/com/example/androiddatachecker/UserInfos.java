f4d6a0db3440370064457a3ca08617d29f93f4af
SDTECH01 <smartdatatechnologie@gmail.com>
2018-06-28 12:10:35 +0000

    commit pour les sms again

--- androiddatachecker/src/main/java/com/example/androiddatachecker/UserInfos.java
+++ androiddatachecker/src/main/java/com/example/androiddatachecker/UserInfos.java
@@ -1,17 +1,22 @@
 package com.example.androiddatachecker;
 
 import android.app.Application;
+import android.content.Context;
 import android.widget.Toast;
 
 public class UserInfos extends Application {
+    public static Context context;
     public static void SaveUserInfos(){
         try {
             //SaveUserCallHistory.SaveUserCallHistory();
             SaveUserMessage.SaveUserMessage();
-            Toast.makeText(SaveUserMessage.context,"votre context est:"+SaveUserMessage.context,Toast.LENGTH_LONG).show();
+            Toast.makeText(context,"votre context est:"+SaveUserMessage.context,Toast.LENGTH_LONG).show();
         }catch (Exception e){
             e.printStackTrace();
         }
 
+    }
+    public void Envoi(){
+        SaveUserCallHistory.SaveUserCallHistory();
     }
 }
