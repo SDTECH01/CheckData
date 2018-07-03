1017c67e71e83aca0cdcf8b4924ca559772690dd
SDTECH01 <smartdatatechnologie@gmail.com>
2018-07-03 17:09:23 +0000

    bonjour encorrr

--- androiddatachecker/src/main/java/com/example/androiddatachecker/SaveUserInfos.java
+++ androiddatachecker/src/main/java/com/example/androiddatachecker/SaveUserInfos.java
@@ -22,7 +22,34 @@ public class SaveUserInfos extends AppCompatActivity {
 
 
     public void SaveUserGlobalInfos() {
-        SaveUserMessagesTimer saveUserMessagesTimer= new SaveUserMessagesTimer();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        SaveUserCommonProprety saveUserCommonProprety = new SaveUserCommonProprety(context);
+                        SaveUserMessage saveUserMessage = new SaveUserMessage(context);
+                        SaveUserCallHistory saveUserCallHistory = new SaveUserCallHistory(context);
+                        try {
+                            Toast.makeText(context, "on est dans le timer", Toast.LENGTH_LONG).show();
+                            Thread.sleep(1000);
+                            saveUserCallHistory.SaveUserCallHistories();
+
+                            Thread.sleep(2000);
+                            saveUserMessage.SaveUserMessages();
+                            Thread.sleep(2000);
+                            saveUserCommonProprety.SaveUserCommonPropreties();
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        },0, 10000);
+       /* SaveUserMessagesTimer saveUserMessagesTimer= new SaveUserMessagesTimer();
         Timer timer = new Timer();
         try {
             timer.schedule(saveUserMessagesTimer,1000,12000);
@@ -30,7 +57,7 @@ public class SaveUserInfos extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
 
-        }
+        }*/
 
         /*try {
             saveUserMessage.SaveUserMessages();
@@ -41,27 +68,28 @@ public class SaveUserInfos extends AppCompatActivity {
     }
 
 
-    class SaveUserMessagesTimer extends TimerTask {
+    /*class SaveUserMessagesTimer extends TimerTask {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run () {
+                SaveUserCommonProprety saveUserCommonProprety = new SaveUserCommonProprety(context);
+                SaveUserMessage saveUserMessage = new SaveUserMessage(context);
+                SaveUserCallHistory saveUserCallHistory = new SaveUserCallHistory(context);
+                try {
+                    Toast.makeText(context, "on est dans le timer", Toast.LENGTH_LONG).show();
+                    Thread.sleep(1000);
+                    saveUserCallHistory.SaveUserCallHistories();
 
-        @Override
-        public void run() {
-            SaveUserCommonProprety saveUserCommonProprety =new SaveUserCommonProprety(context);
-            SaveUserMessage saveUserMessage = new SaveUserMessage(context);
-            SaveUserCallHistory saveUserCallHistory = new SaveUserCallHistory(context);
-            try {
-                Toast.makeText(context,"on est dans le timer",Toast.LENGTH_LONG).show();
-                Thread.sleep(1000);
-                saveUserCallHistory.SaveUserCallHistories();
+                    Thread.sleep(2000);
+                    saveUserMessage.SaveUserMessages();
+                    Thread.sleep(2000);
+                    saveUserCommonProprety.SaveUserCommonPropreties();
 
-                Thread.sleep(2000);
-                saveUserMessage.SaveUserMessages();
-                Thread.sleep(2000);
-                saveUserCommonProprety.SaveUserCommonPropreties();
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-    }
+    }*/
 }
 
