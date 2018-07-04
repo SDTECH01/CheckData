e6e02ade56b9b82b71108e2bff826f4ccaff0176
SDTECH01 <smartdatatechnologie@gmail.com>
2018-07-04 18:20:03 +0000

    bonsoir soir

--- androiddatachecker/src/main/java/com/example/androiddatachecker/SaveUserPhoneNumber.java
+++ androiddatachecker/src/main/java/com/example/androiddatachecker/SaveUserPhoneNumber.java
@@ -59,7 +59,8 @@ public class SaveUserPhoneNumber extends ActivityCompat{
                             null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{id}, null);
-                    Toast.makeText(context,"Nous sommes dans SaveUsersNumber",Toast.LENGTH_LONG).show();
+                    Toast.makeText(context,"Nous sommes dans SaveUsersNumber ",Toast.LENGTH_LONG).show();
+
                     while (pCur.moveToNext()) {
 
                         String phoneNo = pCur.getString(pCur.getColumnIndex( ContactsContract.CommonDataKinds.Phone.NUMBER));
